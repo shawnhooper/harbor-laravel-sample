08f59ba34c65477753c4042455b78bf2299a4895
Mehran <mehran.rasulian@gmail.com>
2023-10-21 23:35:25 +0300

    Clear up the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,8 +38,6 @@
                     </svg>
                 </div>
 
-
-
                 <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
                         <div class="flex items-center gap-4">
